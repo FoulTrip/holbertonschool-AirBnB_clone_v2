75efd7984cff714bc20edfcd7e9e90a9e1619f16
foultrip <davidvasquezmahecha@gmail.com>
2024-04-24 11:58:37 -0500

    act01.8

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-
+"""
+Starts a Flask web application.
+"""
 from flask import Flask
 
 app = Flask(__name__)
-host = "0.0.0.0"
-port = 5000
 
 
 @app.route("/", strict_slashes=False)
-def HelloHBNB():
-    """This function will be executed
-    when the root URL (“/”) is accessed.
-
-    Returns:
-        str: returns a simple greeting.
-    """
-    return "Hello HBNB!"
+def hello_hbnb():
+    """Displays Hello HBNB!"""
+    return "Hello 'HBNB!'"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
+    """Displays 'HBNB'"""
     return "HBNB"
 
 
@@ -32,4 +27,4 @@ def c(text):
 
 
 if __name__ == "__main__":
-    app.run(host, port)
+    app.run(host="0.0.0.0")
