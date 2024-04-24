4959cca1967d36bceebfa6151d0283c23c1a23cb
foultrip <davidvasquezmahecha@gmail.com>
2024-04-24 11:43:32 -0500

    act001.5

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'
 """
+script that starts a Flask web application
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
+host = "0.0.0.0"
+port = 5000
 
 
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+def HelloHBNB():
+    """This function will be executed
+    when the root URL (“/”) is accessed.
+
+    Returns:
+        str: returns a simple greeting.
+    """
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host, port)
